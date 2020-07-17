547c3722b3a1d0b1e95e73b882d7de976fbda15d
Nam Le <nam.leduc02@gmail.com>
2020-07-17 20:12:51 +0200

    1st commit

---
+++ src/MainPanel.tsx
@@ -0,0 +1,292 @@
+import React, { PureComponent } from 'react';
+import { PanelProps } from '@grafana/data';
+import { PanelOptions, Frame } from 'types';
+import { Map, View } from 'ol';
+import XYZ from 'ol/source/XYZ';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { fromLonLat } from 'ol/proj';
+import { defaults, DragPan, MouseWheelZoom } from 'ol/interaction';
+import { platformModifierKeyOnly } from 'ol/events/condition';
+import Select from 'ol/interaction/Select';
+import { Style, Fill } from 'ol/style';
+import { pointerMove } from 'ol/events/condition';
+import { SelectEvent } from 'ol/interaction/Select';
+import { createPolygonLayer, processTransitionData, createHeatInfo } from './utils/helpers';
+import { nanoid } from 'nanoid';
+import 'ol/ol.css';
+
+interface Props extends PanelProps<PanelOptions> {}
+interface State {
+  currentPolygon: string | null;
+}
+
+export class MainPanel extends PureComponent<Props, State> {
+  id = 'id' + nanoid();
+  map1: Map;
+  map2: Map;
+  randomTile1: TileLayer;
+  randomTile2: TileLayer;
+  infoMap1: VectorLayer;
+  infoMap2: VectorLayer;
+  startObj: { [key: string]: { [key: string]: number } } | null;
+  destObj: { [key: string]: { [key: string]: number } } | null;
+
+  state: State = {
+    currentPolygon: null,
+  };
+
+  componentDidMount() {
+    const { tile_url1, tile_url2, zoom_level, center_lon, center_lat, geojson1, geojson2 } = this.props.options;
+
+    const carto1 = new TileLayer({
+      source: new XYZ({
+        url: 'https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      }),
+    });
+    const carto2 = new TileLayer({
+      source: new XYZ({
+        url: 'https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      }),
+    });
+
+    this.map1 = new Map({
+      interactions: defaults({ dragPan: false, mouseWheelZoom: false, onFocusOnly: true }).extend([
+        new DragPan({
+          condition: function(event) {
+            return platformModifierKeyOnly(event) || this.getPointerCount() === 2;
+          },
+        }),
+        new MouseWheelZoom({
+          condition: platformModifierKeyOnly,
+        }),
+      ]),
+      layers: [carto1],
+      view: new View({
+        center: fromLonLat([center_lon, center_lat]),
+        zoom: zoom_level,
+      }),
+      target: this.id + '1',
+    });
+
+    this.map2 = new Map({
+      interactions: defaults({ dragPan: false, mouseWheelZoom: false, onFocusOnly: true }).extend([
+        new DragPan({
+          condition: function(event) {
+            return platformModifierKeyOnly(event) || this.getPointerCount() === 2;
+          },
+        }),
+        new MouseWheelZoom({
+          condition: platformModifierKeyOnly,
+        }),
+      ]),
+      layers: [carto2],
+      view: new View({
+        center: fromLonLat([center_lon, center_lat]),
+        zoom: zoom_level,
+      }),
+      target: this.id + '2',
+    });
+
+    if (tile_url1 !== '') {
+      this.randomTile1 = new TileLayer({
+        source: new XYZ({
+          url: tile_url1,
+        }),
+        zIndex: 1,
+      });
+      this.map1.addLayer(this.randomTile1);
+    }
+
+    if (tile_url2 !== '') {
+      this.randomTile2 = new TileLayer({
+        source: new XYZ({
+          url: tile_url2,
+        }),
+        zIndex: 1,
+      });
+      this.map2.addLayer(this.randomTile2);
+    }
+
+    if (this.props.data.series.length > 0 && geojson1 && geojson2) {
+      const { buildingLayer1, buildingLayer2 } = createPolygonLayer(geojson1, geojson2);
+
+      this.map1.addLayer(buildingLayer1);
+      this.map2.addLayer(buildingLayer2);
+      const serie = this.props.data.series[0] as Frame;
+      if (serie.fields[0].values.buffer.length > 0) {
+        const { startObj, destObj } = processTransitionData(serie.fields[0].values.buffer);
+        this.startObj = startObj;
+        this.destObj = destObj;
+      }
+    }
+
+    const hoverInteraction1 = new Select({
+      condition: pointerMove,
+      style: function(feature) {
+        const style: { [key: string]: any[] } = {};
+        const geometry_type = feature.getGeometry().getType();
+
+        style['Polygon'] = [
+          new Style({
+            fill: new Fill({
+              color: '#ffffff00',
+            }),
+          }),
+        ];
+
+        return style[geometry_type];
+      },
+    });
+
+    const hoverInteraction2 = new Select({
+      condition: pointerMove,
+      style: function(feature) {
+        const style: { [key: string]: any[] } = {};
+        const geometry_type = feature.getGeometry().getType();
+
+        style['Polygon'] = [
+          new Style({
+            fill: new Fill({
+              color: '#ffffff00',
+            }),
+          }),
+        ];
+
+        return style[geometry_type];
+      },
+    });
+
+    hoverInteraction1.on('select', (e: SelectEvent) => {
+      const selectedFeature = e.target.getFeatures().item(0);
+
+      if (selectedFeature) {
+        if (selectedFeature.get('label') !== this.state.currentPolygon) {
+          this.setState({ currentPolygon: selectedFeature.get('label') });
+        }
+      } else {
+        this.setState({ currentPolygon: null });
+      }
+    });
+
+    hoverInteraction2.on('select', (e: SelectEvent) => {
+      const selectedFeature = e.target.getFeatures().item(0);
+
+      if (selectedFeature) {
+        if (selectedFeature.get('label') !== this.state.currentPolygon) {
+          this.setState({ currentPolygon: selectedFeature.get('label') });
+        }
+      } else {
+        this.setState({ currentPolygon: null });
+      }
+    });
+
+    this.map1.addInteraction(hoverInteraction1);
+    this.map2.addInteraction(hoverInteraction2);
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (prevProps.data.series !== this.props.data.series) {
+      this.setState({ currentPolygon: null });
+      if (this.props.options.geojson1 && this.props.options.geojson2) {
+        this.map1.removeLayer(this.infoMap1);
+        this.map2.removeLayer(this.infoMap2);
+
+        if (this.props.data.series.length > 0) {
+          const serie = this.props.data.series[0] as Frame;
+
+          if (serie.fields[0].values.buffer.length > 0) {
+            const { startObj, destObj } = processTransitionData(serie.fields[0].values.buffer);
+            this.startObj = startObj;
+            this.destObj = destObj;
+          }
+        } else {
+          this.startObj = null;
+          this.destObj = null;
+        }
+      }
+    }
+
+    if (prevProps.options.tile_url1 !== this.props.options.tile_url1) {
+      this.map1.removeLayer(this.randomTile1);
+      if (this.props.options.tile_url1 !== '') {
+        this.randomTile1 = new TileLayer({
+          source: new XYZ({
+            url: this.props.options.tile_url1,
+          }),
+          zIndex: 1,
+        });
+        this.map1.addLayer(this.randomTile1);
+      }
+    }
+
+    if (prevProps.options.tile_url2 !== this.props.options.tile_url2) {
+      this.map2.removeLayer(this.randomTile2);
+      if (this.props.options.tile_url2 !== '') {
+        this.randomTile2 = new TileLayer({
+          source: new XYZ({
+            url: this.props.options.tile_url2,
+          }),
+          zIndex: 1,
+        });
+        this.map2.addLayer(this.randomTile2);
+      }
+    }
+
+    if (prevProps.options.zoom_level !== this.props.options.zoom_level) {
+      this.map1.getView().setZoom(this.props.options.zoom_level);
+      this.map2.getView().setZoom(this.props.options.zoom_level);
+    }
+
+    if (
+      prevProps.options.center_lat !== this.props.options.center_lat ||
+      prevProps.options.center_lon !== this.props.options.center_lon
+    ) {
+      this.map1.getView().animate({
+        center: fromLonLat([this.props.options.center_lon, this.props.options.center_lat]),
+        duration: 2000,
+      });
+      this.map2.getView().animate({
+        center: fromLonLat([this.props.options.center_lon, this.props.options.center_lat]),
+        duration: 2000,
+      });
+    }
+
+    if (
+      this.props.options.geojson1 &&
+      this.props.options.geojson2 &&
+      prevState.currentPolygon !== this.state.currentPolygon
+    ) {
+      this.map1.removeLayer(this.infoMap1);
+      this.map2.removeLayer(this.infoMap2);
+      if (!this.state.currentPolygon) {
+        return;
+      }
+      const currentStore = this.state.currentPolygon;
+
+      if (this.startObj && this.destObj && (this.startObj[currentStore] || this.destObj[currentStore])) {
+        const { infoMap1, infoMap2 } = createHeatInfo(
+          this.props.options.geojson1,
+          this.props.options.geojson2,
+          this.startObj[currentStore],
+          this.destObj[currentStore]
+        );
+        this.infoMap1 = infoMap1;
+        this.infoMap2 = infoMap2;
+
+        this.map1.addLayer(this.infoMap1);
+        this.map2.addLayer(this.infoMap2);
+      }
+    }
+  }
+
+  render() {
+    const { width, height } = this.props;
+
+    return (
+      <div style={{ display: 'flex' }}>
+        <div id={this.id + '1'} style={{ width: width / 2, height }}></div>
+        <div id={this.id + '2'} style={{ width: width / 2, height }}></div>
+      </div>
+    );
+  }
+}
---
+++ src/utils/helpers.ts
@@ -0,0 +1,261 @@
+import { Vector as VectorLayer } from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Polygon from 'ol/geom/Polygon';
+import { Style, Fill, Stroke, Text } from 'ol/style';
+import { GeoJSON, FeatureGeojson } from '../types';
+import centroid from '@turf/centroid';
+import Point from 'ol/geom/Point';
+
+const percentageToHsl = (percentage: number) => {
+  const hue = percentage * -120 + 120;
+  return 'hsla(' + hue + ', 100%, 50%, 0.3)';
+};
+
+const measureObj = (obj: { [key: string]: number }) => {
+  const max = Math.max(...Object.values(obj));
+  const min = Math.min(...Object.values(obj));
+  const range = max - min;
+  return {
+    min,
+    range,
+  };
+};
+
+export const createTransparentPolygon = (coordinates: number[][][], label: string) => {
+  const polygonFeature = new Feature({
+    type: 'Polygon',
+    geometry: new Polygon(coordinates).transform('EPSG:4326', 'EPSG:3857'),
+  });
+  polygonFeature.set('label', label);
+  polygonFeature.setStyle(
+    new Style({
+      fill: new Fill({
+        color: '#ffffff00',
+      }),
+    })
+  );
+  return polygonFeature;
+};
+
+export const createPolygonInfo = (feature: FeatureGeojson, label: string, color: string) => {
+  const centerCoord = centroid(feature).geometry.coordinates;
+  const polygonFeature = new Feature({
+    type: 'Polygon',
+    geometry: new Polygon(feature.geometry.coordinates).transform('EPSG:4326', 'EPSG:3857'),
+  });
+
+  polygonFeature.setStyle([
+    new Style({
+      fill: new Fill({
+        color: color,
+      }),
+    }),
+    new Style({
+      geometry: new Point(centerCoord).transform('EPSG:4326', 'EPSG:3857'),
+      text: new Text({
+        stroke: new Stroke({
+          color: '#fff',
+          width: 3,
+        }),
+        font: '15px Calibri,sans-serif',
+        text: label,
+      }),
+    }),
+  ]);
+  return polygonFeature;
+};
+
+export const createPolygonLayer = (geojson1: GeoJSON, geojson2: GeoJSON) => {
+  const polygons1: Feature[] = [];
+  const polygons2: Feature[] = [];
+
+  geojson1.features.map(feature => {
+    if (feature.properties && feature.properties.name && feature.geometry.type == 'Polygon') {
+      polygons1.push(createTransparentPolygon(feature.geometry.coordinates, feature.properties.name));
+    }
+  });
+
+  geojson2.features.map(feature => {
+    if (feature.properties && feature.properties.name && feature.geometry.type == 'Polygon') {
+      polygons2.push(createTransparentPolygon(feature.geometry.coordinates, feature.properties.name));
+    }
+  });
+
+  return {
+    buildingLayer1: new VectorLayer({
+      source: new VectorSource({
+        features: polygons1,
+      }),
+      zIndex: 2,
+    }),
+    buildingLayer2: new VectorLayer({
+      source: new VectorSource({
+        features: polygons2,
+      }),
+      zIndex: 2,
+    }),
+  };
+};
+
+export const createHeatInfo = (
+  geojson1: GeoJSON,
+  geojson2: GeoJSON,
+  startObj: { [key: string]: number } | undefined,
+  destObj: { [key: string]: number } | undefined
+) => {
+  const combineObj: { [key: string]: number } = {};
+
+  const infoMap1Feature: Feature[] = [];
+  const infoMap2Feature: Feature[] = [];
+  if (startObj && !destObj) {
+    Object.keys(startObj).map(store => {
+      combineObj[store] = Math.log2(startObj[store]);
+    });
+
+    const { min, range } = measureObj(combineObj);
+
+    const listDestinations = Object.keys(startObj);
+    geojson1.features.map(feature => {
+      if (feature.properties && feature.properties.name && listDestinations.includes(feature.properties.name)) {
+        infoMap1Feature.push(
+          createPolygonInfo(
+            feature,
+            `To ${startObj[feature.properties.name]}`,
+            percentageToHsl((combineObj[feature.properties.name] - min) / range)
+          )
+        );
+      }
+    });
+
+    geojson2.features.map(feature => {
+      if (feature.properties && feature.properties.name && listDestinations.includes(feature.properties.name)) {
+        infoMap2Feature.push(
+          createPolygonInfo(
+            feature,
+            `To ${startObj[feature.properties.name]}`,
+            percentageToHsl((combineObj[feature.properties.name] - min) / range)
+          )
+        );
+      }
+    });
+  } else if (!startObj && destObj) {
+    Object.keys(destObj).map(store => {
+      combineObj[store] = Math.log2(destObj[store]);
+    });
+    const { min, range } = measureObj(combineObj);
+
+    const listSources = Object.keys(destObj);
+
+    geojson1.features.map(feature => {
+      if (feature.properties && feature.properties.name && listSources.includes(feature.properties.name)) {
+        infoMap1Feature.push(
+          createPolygonInfo(
+            feature,
+            `From ${destObj[feature.properties.name]}`,
+            percentageToHsl((combineObj[feature.properties.name] - min) / range)
+          )
+        );
+      }
+    });
+    geojson2.features.map(feature => {
+      if (feature.properties && feature.properties.name && listSources.includes(feature.properties.name)) {
+        infoMap2Feature.push(
+          createPolygonInfo(
+            feature,
+            `From ${destObj[feature.properties.name]}`,
+            percentageToHsl((combineObj[feature.properties.name] - min) / range)
+          )
+        );
+      }
+    });
+  } else if (startObj && destObj) {
+    Object.keys(startObj).map(store => {
+      if (destObj[store]) {
+        combineObj[store] = Math.log2(startObj[store] + destObj[store]);
+      } else {
+        combineObj[store] = Math.log2(startObj[store]);
+      }
+    });
+
+    Object.keys(destObj).map(store => {
+      if (!startObj[store]) {
+        combineObj[store] = Math.log2(destObj[store]);
+      }
+    });
+    const { min, range } = measureObj(combineObj);
+
+    const listDestinations = Object.keys(startObj);
+    const listSources = Object.keys(destObj);
+    const allRelatedStores = [...new Set([...listDestinations, ...listSources])];
+
+    geojson1.features.map(feature => {
+      if (feature.properties && feature.properties.name && allRelatedStores.includes(feature.properties.name)) {
+        const label =
+          `${startObj[feature.properties.name] ? `To ${startObj[feature.properties.name]}` : ''}` +
+          `${destObj[feature.properties.name] ? ` From ${destObj[feature.properties.name]}` : ''}`;
+
+        infoMap1Feature.push(
+          createPolygonInfo(feature, label, percentageToHsl((combineObj[feature.properties.name] - min) / range))
+        );
+      }
+    });
+
+    geojson2.features.map(feature => {
+      if (feature.properties && feature.properties.name && allRelatedStores.includes(feature.properties.name)) {
+        const label =
+          `${startObj[feature.properties.name] ? `To ${startObj[feature.properties.name]}` : ''}` +
+          `${destObj[feature.properties.name] ? ` From ${destObj[feature.properties.name]}` : ''}`;
+
+        infoMap2Feature.push(
+          createPolygonInfo(feature, label, percentageToHsl((combineObj[feature.properties.name] - min) / range))
+        );
+      }
+    });
+  }
+
+  return {
+    infoMap1: new VectorLayer({
+      source: new VectorSource({
+        features: infoMap1Feature,
+      }),
+      zIndex: 3,
+    }),
+    infoMap2: new VectorLayer({
+      source: new VectorSource({
+        features: infoMap2Feature,
+      }),
+      zIndex: 3,
+    }),
+  };
+};
+
+export const processTransitionData = (data: any[]) => {
+  const excludeArr = ['_id', '_index', '_type', 'Source', 'timestamp'];
+  const startObj: { [key: string]: { [key: string]: number } } = {};
+  const destObj: { [key: string]: { [key: string]: number } } = {};
+
+  data.map(row => {
+    if (!startObj[row.Source]) startObj[row.Source] = {};
+
+    Object.keys(row).map(destination => {
+      if (!excludeArr.includes(destination) && row[destination] > 0) {
+        startObj[row.Source][destination]
+          ? (startObj[row.Source][destination] += row[destination])
+          : (startObj[row.Source][destination] = row[destination]);
+
+        if (!destObj[destination]) destObj[destination] = {};
+
+        destObj[destination][row.Source]
+          ? (destObj[destination][row.Source] += row[destination])
+          : (destObj[destination][row.Source] = row[destination]);
+      }
+    });
+  });
+
+  Object.keys(startObj).map(start => {
+    if (Object.keys(start).length == 0) delete startObj[start];
+  });
+
+  return { startObj, destObj };
+};
